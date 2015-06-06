673d15d0c00dd1f4cca027fdf69cabc89eeca2f6
ming xie <davidxiem@gmail.com>
2015-06-06 22:07:19 +1000

    init a job form

--- src/main/java/com/webessay/controller/Jobform.java
+++ src/main/java/com/webessay/controller/Jobform.java
@@ -1,11 +1,15 @@
 package com.webessay.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.webessay.model.Jobs;
 
 @RequestMapping("/jobform/**")
 @Controller
@@ -16,7 +20,9 @@ public class Jobform {
     }
 
     @RequestMapping
-    public String index() {
+    public String index(Model uiModel) {
+    	Jobs jobs = new Jobs();
+    	uiModel.addAttribute("jobs", jobs);
         return "jobform/index";
     }
 }
