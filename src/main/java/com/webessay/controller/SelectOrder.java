894216d11107f068860f959e72c5b822dc3012a6
ming xie <davidxiem@gmail.com>
2015-06-07 09:35:17 +1000

    run roo command controller selectorder.form

--- src/main/java/com/webessay/controller/SelectOrder.java
+++ src/main/java/com/webessay/controller/SelectOrder.java
@@ -1,29 +1,22 @@
 package com.webessay.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.webessay.model.Jobs;
-
-@RequestMapping("/jobform/**")
+@RequestMapping("/selectorder/**")
 @Controller
-public class Jobform {
+public class SelectOrder {
 
     @RequestMapping(method = RequestMethod.POST, value = "{id}")
     public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
     }
 
-    @RequestMapping()
-    public String index(Model uiModel, @RequestParam(value = "orderid") Integer orderid) {
-    	Jobs jobs = new Jobs();
-    	uiModel.addAttribute("jobs", jobs);
-        return "jobform/index";
+    @RequestMapping
+    public String index() {
+        return "selectorder/index";
     }
 }
