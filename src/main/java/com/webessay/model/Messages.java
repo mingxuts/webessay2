399ee115db6e2405689b2d15f0ed904714cdd871
ming xie <davidxiem@gmail.com>
2015-06-07 15:14:46 +1000

    add compose msg controller

--- src/main/java/com/webessay/model/Messages.java
+++ src/main/java/com/webessay/model/Messages.java
@@ -1,13 +1,11 @@
 package com.webessay.model;
 import java.util.Calendar;
 import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.roo.addon.dbre.RooDbManaged;
 import org.springframework.roo.addon.javabean.RooJavaBean;
@@ -19,25 +17,25 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooDbManaged(automaticallyDelete = true)
 @RooJpaEntity(versionField = "", table = "messages")
 public class Messages {
-	
-	@PreUpdate
-	@PrePersist
-	public void updateTimeStamps() {
-		if (this.date == null){
-			this.date = new Date();
-		}
-	}
 
-	@Column(name = "Date")
+    @PreUpdate
+    @PrePersist
+    public void updateTimeStamps() {
+        if (this.date == null) {
+            this.date = new Date();
+        }
+    }
+
+    @Column(name = "Date")
     @Temporal(TemporalType.TIMESTAMP)
     @DateTimeFormat(style = "MM")
     private Date date;
 
-	public Date getDate() {
+    public Date getDate() {
         return date;
     }
 
-	public void setDate(Date date) {
+    public void setDate(Date date) {
         this.date = date;
     }
 }
