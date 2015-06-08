2ffa67fcad0e5d549e50b304dcbaa425ca83cd69
ming xie <davidxiem@gmail.com>
2015-06-08 14:51:03 +1000

    add audit messages controller

--- src/main/java/com/webessay/model/Jobs.java
+++ src/main/java/com/webessay/model/Jobs.java
@@ -1,6 +1,5 @@
 package com.webessay.model;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.roo.addon.dbre.RooDbManaged;
