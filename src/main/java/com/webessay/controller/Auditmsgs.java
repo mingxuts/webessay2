ffe22f33ccd2a2a5cc54d0cc9b35ddf27207c7d8
ming xie <davidxiem@gmail.com>
2015-06-11 18:10:47 +1000

    edit message done

--- src/main/java/com/webessay/controller/Auditmsgs.java
+++ src/main/java/com/webessay/controller/Auditmsgs.java
@@ -11,6 +11,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.webessay.model.Messages;
 import com.webessay.model.MessagesRepository;
@@ -26,7 +27,7 @@ public class Auditmsgs {
     public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
     }
 
-    @RequestMapping
+    @RequestMapping(value = "/index")
     public String index(Model uiModel) {    	
     	Boolean hasAdudit = false;
     	Messages msg = new Messages();
@@ -34,4 +35,30 @@ public class Auditmsgs {
 		uiModel.addAttribute("msglist", msglist);
         return "auditmsgs/index";
     }
+    
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public String edit(Model uiModel, @RequestParam Integer id){
+    	Messages msg = repo.findOne(id);
+    	Integer msgid = msg.getId();
+    	uiModel.addAttribute("msgid", msgid);
+    	
+    	String text = msg.getText();
+    	uiModel.addAttribute("oldtext", text);
+    	
+    	String auditText = msg.getAuditText();
+    	uiModel.addAttribute("auditText", auditText);
+		return "auditmsgs/edit";
+    	
+    }
+    
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public String editPost(@RequestParam("msgid") Integer id, @RequestParam("auditText") String auditText){
+    	Messages msg = repo.findOne(id);
+    	msg.setAuditText(auditText);
+    	msg.setHasAdudit(true);
+    	repo.save(msg);
+		return "redirect:auditmsgs/index";
+    	
+    }
+    
 }
